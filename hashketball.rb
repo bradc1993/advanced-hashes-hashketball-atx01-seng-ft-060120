f2b249c80285dc0fcc7ca67520a102832ecab3f1
Brad Carter <bradc1993@gmail.com>
2020-05-22 04:32:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,7 +204,7 @@ end
 def big_shoe_rebounds
   game_hash.each do |team, section|
     section.each do |team_colors_players, stats|
-      team_colors_players[:players].max_by { |name, shoe| stats[:shoe] }.last[:shoe]
+      section[:players].max_by { |name, shoe| stats[:shoe] }.last[:shoe]
     end
   end
 end
